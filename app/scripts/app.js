4d0318c4298c94895bcdfcdf161a1ecce594af83
achoraev <audulin@gmail.com>
2014-09-28 15:56:27 +0300

    build 28092014

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,6 +19,10 @@
                     targetWidth: 400
                 };
                 navigator.camera.getPicture(success, error, config);
+            },
+
+            deleteAll: function () {
+                // todo
             }
         });
         var app = new kendo.mobile.Application(document.body, {
@@ -33,16 +37,52 @@
                 });
                 $("#images").kendoMobileListView({
                     dataSource: files,
-                    template: "<img src='#: data #'>"              
-                    // template: $("#customTemplate").html()
+                    //template: "<img src='#: data #'>"              
+                    template: $("#customTemplate").html()
                 });
             });
         }
         loadPhotos();
+        
+        var getYear = (function () {
+            return new Date().getFullYear();
+        }());
 
+        var showConfirm = function (message, title, callback) {
+            navigator.notification.confirm(message, callback || function () {
+            }, title, ['OK', 'Cancel']);
+        };
+
+        var isNullOrEmpty = function (value) {
+            return typeof value === 'undefined' || value === null || value === '';
+        };
+
+        var isKeySet = function (key) {
+            var regEx = /^\$[A-Z_]+\$$/;
+            return !isNullOrEmpty(key) && !regEx.test(key);
+        };
+
+        var fixViewResize = function () {
+            if (device.platform === 'iOS') {
+                setTimeout(function () {
+                    $(document.body).height(window.innerHeight);
+                }, 10);
+            }
+        };
         // document.addEventListener("click", function() {
         //     $('btn-delete').navigator.notification.alert('test')
 
         // })
     });
+
+    return {
+        //showAlert: showAlert,
+        //showError: showError,
+        showConfirm: showConfirm,
+        isKeySet: isKeySet,
+        //mobileApp: mobileApp,
+        //helper: AppHelper,
+        everlive: el,
+        getYear: getYear
+    }
 }());
