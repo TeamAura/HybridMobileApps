9509aa6fff0a9166820a33ee787edab8922772e0
krumov <krasin.krumov@gmail.com>
2014-09-29 18:15:51 +0300

    added basic todo

---
+++ AuraTeam/scripts/app/add-todo.js
@@ -0,0 +1,21 @@
+/* global kendo, window */
+
+var app = app || {};
+app.viewmodels = app.viewmodels || {};
+
+(function (scope) {
+    'use strict';
+    scope.addTodo = kendo.observable({
+        title: '',
+        isUrgent: false,
+        saveTodo: function () {
+            //backend serves
+            window.todos.push({
+                title: this.get('title'),
+                isUrgent: this.get('isUrgent')
+            });
+
+            app.mobileApp.navigate('views/todo.html');
+        }
+    });
+}(app.viewmodels));
