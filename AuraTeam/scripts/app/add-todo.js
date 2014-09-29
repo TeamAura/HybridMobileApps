9a866598dde864e6a74e66adcd17ebb37fc2f35d
achoraev <audulin@gmail.com>
2014-09-29 20:03:21 +0300

    20.03

--- AuraTeam/scripts/app/add-todo.js
+++ AuraTeam/scripts/app/add-todo.js
@@ -7,27 +7,14 @@ app.viewmodels = app.viewmodels || {};
     'use strict';
     scope.addTodo = kendo.observable({
         title: '',
-<<<<<<< HEAD
-        isUrgent: false,
-        saveTodo: function () {
-            //backend serves
-            window.todos.push({
-                title: this.get('title'),
-                isUrgent: this.get('isUrgent')
-            });
-
-            app.mobileApp.navigate('views/todo.html');
-=======
         content: '',
         saveTodo: function () {
-            //backend serves
-            console.log("test add todo");
+            //backend serves            
             window.todos.push({
                 title: this.get('title'),
                 content: this.get('content')
             });
-
->>>>>>> 1c8068d9c073d764329b04e5f29ee80744fabb26
         }
     });
+       
 }(app.viewmodels));
